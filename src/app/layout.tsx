d5c5875c864af5aa89bcb83da7a0203573d8f32d
Saqlain Riaz <saqlainprinters@gmail.com>
2025-11-01 13:13:09 +0500

    chore: favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./index.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://kickoffmedia.co"),
   title: "KickOff Media - A digital marketing agency",
   description:
     "A leading digital marketing agency offering Social Media Marketing, SEO, Google Ads, Content Creation, Email Marketing, and Data-Driven Growth Strategies. Elevate your brand with our expert team and proven results.",
