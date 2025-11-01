1acc0d4c7e3f2ec7c547c464f420343473e1132f
Saqlain Riaz <saqlainprinters@gmail.com>
2025-11-01 12:38:27 +0500

    chore: default

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export", // Outputs a Single-Page Application (SPA)
-  distDir: "dist", // Changes the build output directory to `build`
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
